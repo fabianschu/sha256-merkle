1ea9fd9b8161dca2d24afbda89a5686fdbd70448
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2023-03-27 23:12:14 +0200

    chore: adds example functions

--- packages/hardhat/contracts/Hasher.sol
+++ packages/hardhat/contracts/Hasher.sol
@@ -10,4 +10,21 @@ contract Hasher {
     function hash(bytes memory x) public pure returns (bytes32) {
         return sha256(x);
     }
+    
+    function concatHash(bytes memory x, bytes memory y) public pure returns (bytes32) {
+        return sha256(abi.encodePacked(x,y));
+    }
+
+    function concatBytes(bytes memory x, bytes memory y) public pure returns (bytes memory) {
+        return abi.encodePacked(x,y);
+    }
+
+    function concatBytes32(bytes32 x, bytes32 y) public pure returns (bytes memory) {
+        return abi.encodePacked(x,y);
+    }
+
+
+    function concatBytes32Hash(bytes32 x, bytes32 y) public pure returns (bytes32) {
+        return sha256(abi.encodePacked(x, y));
+    }
 }
--- packages/hardhat/test/Hasher.js
+++ packages/hardhat/test/Hasher.js
@@ -6,7 +6,7 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Lock", function () {
+describe("Hasher", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
@@ -20,26 +20,63 @@ describe("Lock", function () {
     return { hasher, owner, otherAccount };
   }
 
+  const addresses = [
+    "0x9cA70B93CaE5576645F5F069524A9B9c3aef5006",
+    "0xb5aE5169F4D750e802884d81b4f9eC66c525396F",
+  ];
+  const b32 = addresses.map((a) => ethers.utils.sha256(a));
+
   describe("#hash", function () {
-    it("returns hash", async function () {
-      const expected =
-        "0x66687aadf862bd776c8fc18b8e9f8e20089714856ee233b3902a591d0d5f2925";
+    const address = "0x9cA70B93CaE5576645F5F069524A9B9c3aef5006";
+
+    it("equals ethers sha256", async function () {
       const { hasher } = await loadFixture(deployOneYearLockFixture);
 
-      console.log(await ethers.utils.arrayify(ethers.constants.HashZero));
-      console.log(
-        await ethers.utils.arrayify(
-          "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
-        )
+      const solidityHash = await hasher.hash(address);
+      const ethersHash = await ethers.utils.sha256(address);
+
+      expect(solidityHash).to.equal(ethersHash);
+    });
+  });
+
+  describe("#concatBytes", function () {
+    it("produces same hash as ethers", async () => {
+      const { hasher } = await loadFixture(deployOneYearLockFixture);
+
+      const solidityHash = await hasher.concatBytes(...addresses);
+      const ethersHash = await ethers.utils.solidityPack(
+        ["bytes", "bytes"],
+        addresses
       );
 
-      const result = await hasher.hash(ethers.constants.HashZero);
+      expect(solidityHash).to.equal(ethersHash);
+    });
+  });
 
-      // expect(result).to.equal(
-      //   await ethers.utils.hexlify(
-      //     "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
-      //   )
-      // );
+  describe("#concatBytes32", function () {
+    it("produces same hash as ethers", async () => {
+      const { hasher } = await loadFixture(deployOneYearLockFixture);
+
+      const solidityHash = await hasher.concatBytes(...b32);
+      const ethersHash = await ethers.utils.solidityPack(
+        ["bytes32", "bytes32"],
+        b32
+      );
+
+      expect(solidityHash).to.equal(ethersHash);
+    });
+  });
+
+  describe("#concatBytes32Hash", () => {
+    it("produces same hash as ethers", async () => {
+      const { hasher } = await loadFixture(deployOneYearLockFixture);
+
+      const solidityHash = await hasher.concatBytes32Hash(...b32);
+      const ethersHash = ethers.utils.sha256(
+        await ethers.utils.solidityPack(["bytes32", "bytes32"], b32)
+      );
+
+      expect(solidityHash).to.equal(ethersHash);
     });
   });
 });
